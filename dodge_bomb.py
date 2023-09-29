5e8cb3b6359cd6766fea0f52ef938eca42c22a45
noa <c0b2213790@edu.teu.ac.jp>
2023-09-29 16:33:50 +0900

    練習3

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -12,22 +12,40 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = 900,400
     enn = pg.Surface((20, 20))
     pg.draw.circle(enn, (255, 0, 0), (10, 10), 10)
     enn.set_colorkey((0, 0, 0))
     x, y = random.randint(0,1600), random.randint(0,900)
-    vx,vy = +5,+5
+    vx,vy = +5, +5
     enn_rct = enn.get_rect()
     enn_rct.center = x, y
     clock = pg.time.Clock()
     tmr = 0
+    key_zi = {
+        pg.K_UP: (0,-5),
+        pg.K_DOWN: (0,+5),
+        pg.K_LEFT: (-5,0),
+        pg.K_RIGHT: (+5,0),
+        }
+    
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
 
         screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, [900, 400])
+        screen.blit(kk_img, [kk_rct.x, kk_rct.y])
+        
+        key_lst = pg.key.get_pressed()
+        total = [0, 0]
+        for k, m in key_zi.items():
+            if key_lst[k]:
+                total[0] += m[0]
+                total[1] += m[1]
+        kk_rct.move_ip(total)
+        
         screen.blit(enn, [enn_rct.x, enn_rct.y])
         enn_rct.move_ip(vx,vy)
         pg.display.update()
