bb6fa97ace93017cfc7a0a7cc91851105546967a
noa <c0b2213790@edu.teu.ac.jp>
2023-09-30 19:48:50 +0900

    追加機能1

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -16,6 +16,7 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    hankk = pg.transform.flip(kk_img, True, False)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 900,400
     enn = pg.Surface((20, 20))
@@ -28,11 +29,21 @@ def main():
     clock = pg.time.Clock()
     tmr = 0
     key_zi = {
-        pg.K_UP: (0,-5),
-        pg.K_DOWN: (0,+5),
-        pg.K_LEFT: (-5,0),
-        pg.K_RIGHT: (+5,0),
+        pg.K_UP: (0, -5),
+        pg.K_DOWN: (0, +5),
+        pg.K_LEFT: (-5, 0),
+        pg.K_RIGHT: (+5, 0),
         }
+    cos_zi = {
+        (0, 5): (pg.transform.flip(kk_img, True, False), -90),
+        (5, 5): (pg.transform.flip(kk_img, True, False), -45),
+        (5, 0): (pg.transform.flip(kk_img, True, False), 0),
+        (5, -5): (pg.transform.flip(kk_img, True, False), 45),
+        (0, -5): (pg.transform.flip(kk_img, True, False), 90),
+        (-5, 5): (kk_img, 45),
+        (-5, -5): (kk_img, -45),
+        (-5, 0): (kk_img, 0)
+    }
     
     while True:
         for event in pg.event.get():
@@ -40,29 +51,36 @@ def main():
                 return
 
         screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, [kk_rct.x, kk_rct.y])
         
         key_lst = pg.key.get_pressed()
         total = [0, 0]
+        newkk = kk_img
+        
         for k, m in key_zi.items():
             if key_lst[k]:
                 total[0] += m[0]
                 total[1] += m[1]
         kk_rct.move_ip(total)
         
+        for kk, mm in cos_zi.items():
+            if total[0] == kk[0] and total[1] == kk[1]:
+                newkk = pg.transform.rotozoom(mm[0], mm[1], 1.0)
+        
         kk_in = gamengai(kk_rct)
         if not kk_in[0]:
             kk_rct.x = max(0, min(kk_rct.x, WIDTH - kk_rct.width))
         if not kk_in[1]:
             kk_rct.y = max(0, min(kk_rct.y, HEIGHT - kk_rct.height))
-
-        screen.blit(enn, [enn_rct.x, enn_rct.y])
+            
+        screen.blit(newkk, [kk_rct.x, kk_rct.y])
 
         enn_in = gamengai(enn_rct)
         if not enn_in[0]:
             vx = -vx
         if not enn_in[1]:
             vy = -vy
+            
+        screen.blit(enn, [enn_rct.x, enn_rct.y])
             
         if kk_rct.colliderect(enn_rct):
             return
